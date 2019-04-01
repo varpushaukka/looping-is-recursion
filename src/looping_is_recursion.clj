e91bff6bbece833d5b824442f8c2a6d2249aca11
Henna Kalliokoski <henna.kalliokoski@siili.com>
2019-04-01 16:47:26 +0300

    part 1, 2 and 3

--- src/looping_is_recursion.clj
+++ src/looping_is_recursion.clj
@@ -1,13 +1,26 @@
 (ns looping-is-recursion)
 
 (defn power [base exp]
-  ":(")
+  (if (zero? exp)
+    1
+    (* base (power base (dec exp)))))
 
 (defn last-element [a-seq]
-  ":(")
+ (if (empty? a-seq)
+   nil
+   (if (== 1 (count a-seq))
+     (first a-seq)
+     (last-element (rest a-seq)))))
 
 (defn seq= [seq1 seq2]
-  ":(")
+  (if (and (empty? seq1) (empty? seq2))
+    true
+    (if (or (empty? seq1) (empty? seq2))
+      false
+      (if (== (first seq1) (first seq2))
+       (seq= (rest seq1) (rest seq2))
+       false
+      ))))
 
 (defn find-first-index [pred a-seq]
   ":(")
